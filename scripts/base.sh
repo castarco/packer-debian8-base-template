44ee53fe4217f63a4d1455404d1343b4fa23ae8a
Daniel Vigueras <daniel@danielvigueras.com>
2015-09-08 16:16:52 +0200

    Remove cdrom line from sources.list

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-perl -p -i -e 's#http://us.archive.ubuntu.com/ubuntu#http://mirror.rackspace.com/ubuntu#gi' /etc/apt/sources.list
+sed -i "/^deb cdrom:/s/^/#/" /etc/apt/sources.list
 
 # Update the box
 apt-get -y update >/dev/null
